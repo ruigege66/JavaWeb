fbd2d5a97c78440dbb1e8bf22d803057536068ba
ruigege66 <1215217867@qq.com>
2020-10-03 20:58:38 +0800

    提交信息

--- chapter03/src/com/qfedu/test/TestDBUtils_ColumnListHandler.java
+++ chapter03/src/com/qfedu/test/TestDBUtils_ColumnListHandler.java
@@ -6,5 +6,12 @@ import org.apache.commons.dbutils.handlers.ColumnListHandler;
 import com.qfedu.utils.C3P0Utils;
 
 public class TestDBUtils_ColumnListHandler {
-
+	public static void main(String[] args) throws SQLException{
+		QueryRunner queryRunner = new QueryRunner(C3P0Utils.getDataSource());
+		
+		String sql = "select * from students";
+		List<Object> list = (List<Object>) queryRunner.query(sql, new ColumnListHandler("s_name"));
+		
+		System.out.println(list);
+	}
 }
--- chapter03/src/com/qfedu/utils/C3P0Utils.java
+++ chapter03/src/com/qfedu/utils/C3P0Utils.java
@@ -1,5 +1,14 @@
 package com.qfedu.utils;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class C3P0Utils {
+	private static ComboPooledDataSource dataSource = new ComboPooledDataSource("qfedu");
+	
+	public static DataSource getDataSource() {
+		return dataSource;
+	}
 
 }
