a70818ae816cbc0655677638e191b2688a233699
ruigege66 <1215217867@qq.com>
2020-10-03 20:22:02 +0800

    提交数据库连接池

---
+++ chapter03/src/com/qfedu/test/TestDBUtils_ColumnListHandler.java
@@ -0,0 +1,10 @@
+package com.qfedu.test;
+import java.sql.SQLException;
+import java.util.List;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ColumnListHandler;
+import com.qfedu.utils.C3P0Utils;
+
+public class TestDBUtils_ColumnListHandler {
+
+}
---
+++ chapter03/src/com/qfedu/utils/C3P0Utils.java
@@ -0,0 +1,5 @@
+package com.qfedu.utils;
+
+public class C3P0Utils {
+
+}
